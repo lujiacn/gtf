25aabcd18689a3f9caca40b3ccd3397c138ce9af
Jia Lu <jia.lu@sanofi.com>
2020-12-04 22:36:57 +0800

    update timeIn format

--- gtf.go
+++ gtf.go
@@ -37,7 +37,7 @@ var GtfFuncMap = template.FuncMap{
 		if err != nil {
 			return t
 		}
-		t = t.In(loc)
+		t = t.In(loc).Format("2006-01-02 15:04 -0700")
 		return t
 	},
 	"funcMap": func(v ...interface{}) []interface{} {
